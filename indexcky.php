d07bf2c213990949a3feffae7aaa59e940033c8e
andres9208 <alg9208@gmail.com>
2022-01-27 15:38:48 -0500

    bootstrap

---
+++ indexcky.php
@@ -0,0 +1,34 @@
+<?php include('head.php'); ?>
+
+
+            <section class="contact-section">               
+                <div class="cus1-bg">
+                  <img src="assets/img/senorpng.png"  alt="image contact us">
+                </div>                  
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-8">
+                            <div class="formulario1">
+                                <h2>Iniciar Sesión</h2>
+                                    <form>
+                                        <div class="campos1">
+                                            <div class="form-control-lg">
+                                                <input type="text" name="usuario" placeholder="Usuario">
+                                            </div>
+                                            <div class=" form-control-lg">
+                                                <input type="text" name="contraseña" placeholder="Contraseña">
+                                            </div>                                                  
+                                        </div>
+                                        <div>
+                                            <input type="submit" class="boton boton1-send" value="Send">
+                                        </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+<?php include('foo.php'); ?>
+    
