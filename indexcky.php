7ec7554f5b92452d301650b01f5b5586c1a5fe14
andres9208 <alg9208@gmail.com>
2022-01-29 14:49:55 -0500

    cambio indexcky

--- indexcky.php
+++ indexcky.php
@@ -1,43 +1,41 @@
 <?php include('head.php'); ?>
 
 
-            <section class="Home">                                   
-                <div class="container">
-                    <div class="row">                        
-                        <div class="col-lg-4">
-                            <div class="cus1-img">
-                                <h2>Maneja la info de tus clientes de una manera fácil y rápida</h2>
-                                <img src="assets/img/senorpng.png"  alt="image contact us">
-                            </div>
-                        </div>
-                        <div class="col-lg-8">
-                            <div class="top__cky">
-                                <img src="assets/img/logofinder.png"  alt="image contact us">
-                                <input type="submit" class="boton boton1-send" value="Registrarme">
-                            </div>  
+    <section class="Home__cky">                                   
+        <div class="container__cky">
+            <div class="row__cky">                        
+                <div class="col-lg-4 izquierda__cky">                            
+                    <h2>Maneja la info de tus clientes de una manera fácil y rápida</h2>
+                    <img src="assets/img/senorpng.png"  alt="image contact us">                            
+                </div>
+                <div class="col-lg-8 derecha__cky">
+                    <div class="derecha__top__cky">
+                        <img src="assets/img/logofinder.png"  alt="image contact us">
+                        <input type="submit" class="boton boton1-send" value="Registrarme">
+                    </div>  
 
-                            <div class="formulario-cky">                                                              
-                                <form action="" method="POST" id="form">
-                                    <div class="form">
-                                        <h1>Iniciar Sesión</h1>
-                                        <div class="grupo">
-                                            <input type="text" name="" id="user" required><span class="barra"></span>
-                                            <label for="">Usuario</label>
-                                        </div>
-                                        
-                                        <div class="grupo">
-                                            <input type="password" name="" id="password" required><span class="barra"></span>
-                                            <label for="">Contraseña</label>
-                                        </div>
+                    <div class="izquierda__formulario__cky">                                                              
+                        <form action="" method="POST" id="form">
+                            <div class="izquierda__form__cky">
+                                <h1>Iniciar Sesión</h1>
+                                <div class="usuario__cky">
+                                    <input type="text" name="" id="user" required><span class="barra"></span>
+                                    <label for="">Usuario</label>
+                                </div>
+                                
+                                <div class="password__cky">
+                                    <input type="password" name="" id="password" required><span class="barra"></span>
+                                    <label for="">Contraseña</label>
+                                </div>
 
-                                        <button class="btn btn-primary" type="submit">Ingresar</button>
-                                    </div>
-                                </form>
+                                <button class="btn btn-primary ingresar__cky" type="submit">Ingresar</button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
-            </section>
+            </div>
+        </div>
+    </section>
 
 <?php include('foo.php'); ?>
     
