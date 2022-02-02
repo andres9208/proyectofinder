3b833c488d73bbd18a81aff143f7a70d0d53d58e
andres9208 <alg9208@gmail.com>
2022-02-02 17:13:49 -0500

    css cky

--- indexcky.php
+++ indexcky.php
@@ -1,42 +1,45 @@
 <?php include('head.php'); ?>
-
-    <section class="Home__cky">                                   
-        <div class="container__cky">
-            <div class="row__cky">  
-
-                <div class="izquierda__cky">                            
-                    <h2 class="textocky">Maneja la info de tus clientes de una manera fácil y rápida</h2>                    
-                    <img src="assets/img/senorpng.png"  alt="image contact us">                                                                   
-                </div>
-
-                <div class="derecha__cky">
-                    <div class="derecha__header__cky">
-                        <img src="assets/img/logofinder.png"  alt="image contact us">
-                        <input type="submit" class="boton__registrarme__cky" value="Registrarme">
-                    </div>  
-
-                    <div class="formulario__cky">                                                              
-                        <form action="" method="POST" id="form">
-                            <div class="form__cky">
-                                <h1>Iniciar Sesión</h1>
-                                <div class="usuario__cky">
-                                    <input type="text" name="" id="user" required><span class="barra"></span>
-                                    <label for="">Usuario</label>
-                                </div>
-                                
-                                <div class="password__cky">
-                                    <input type="password" name="" id="password" required><span class="barra"></span>
-                                    <label for="">Contraseña</label>
-                                </div>
-
-                                <button class="ingresar__cky" type="submit">Ingresar</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                                       
+    <div class="container-cky">
+        <div class="container-cky__izquierda"> 
+            <div class="container-cky__izquierda__inner">                           
+                <h2>Maneja la info de tus clientes de una manera fácil y rápida</h2>                    
+                <img src="assets/img/senorpng.png"  alt="image contact us">   
+            </div>                                                                
         </div>
-    </section>
+
+        <div class="container-cky__derecha">
+            <div class="container-cky__derecha__header">
+                <img src="assets/img/logofinder.png"  alt="image contact us">
+                <input type="submit" class="btn-cky__registro" value="Registrarme">
+            </div>  
+
+            <div class="container-cky__derecha__inner">
+                <h2>Iniciar Sesión</h2> 
+
+                <form class="form-cky">                    
+                    <div class="campo-cky">
+                        <input type="text" name="" id="user" required><span class="barra"></span>
+                        <label>Usuario</label>
+                        <div class="campo-cky__icon">
+                            <img src="./assets/img/samu-icon-user.svg" alt="Gráfico Finder">
+                        </div>
+                    </div>
+                    
+                    <div class="campo-cky">
+                        <input type="password" name="" id="password" required><span class="barra"></span>
+                        <label>Contraseña</label>
+                        <div class="campo-cky__icon">
+                            <img src="./assets/img/samu-icon-password.svg" alt="Gráfico Finder">
+                        </div>
+                    </div>
+
+                    <button class="bnt-cky__ingresar" type="submit">Ingresar</button>                    
+                </form>
+            </div>
+        </div>            
+    </div>
+    
 
 <?php include('foo.php'); ?>
     
